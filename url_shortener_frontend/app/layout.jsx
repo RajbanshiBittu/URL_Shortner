5a85ffbdca356d2d22b8a04b4bfa38dd02f4fcd3
RajbanshiBittu <bitturajbanshi767@gmail.com>
2026-08-05 12:59:51 +0530

    second commit after frontend setup Next.js

---
+++ url_shortener_frontend/app/layout.jsx
@@ -0,0 +1,39 @@
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+import { ThemeProvider } from "@/components/theme-provider";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata = {
+  title: "ShortItOut - A URL Shortener",
+  description: "A URL Shortener built with Next.js, Tailwind.css, and JavaScript",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html
+      lang="en"
+      suppressHydrationWarning
+      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+    >
+      <body className="min-h-full flex flex-col">
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
